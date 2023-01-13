b975547c5be3fc5d53227c254e2c1a8326f6568c
Daniel Agbenyega <50689568+dcujoe@users.noreply.github.com>
2023-01-13 23:34:58 +0100

    updates

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,49 +1,51 @@
 import React from "react";
-import FaBarcode from "react-icons/fa/";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Head() {
   return (
-    <div className="heading" id="heading">
-      <nav className="nav container">
-        <FaBarcode />
-        <div className="nav__menu" id="nav__menu">
-          <ul className="nav__list">
-            <li className="nav_item">
-              <a href="#home" className="nav__link active-link">
-                Home
-              </a>
-            </li>
-            <li className="nav_item">
-              <a href="#home" className="nav__link">
-                Featured
-              </a>
-            </li>
-            <li className="nav_item">
-              <a href="#home" className="nav__link">
-                Products
-              </a>
-            </li>
-            <li className="nav_item">
-              <a href="#home" className="nav__link">
-                New Collection
-              </a>
-            </li>
-          </ul>
+    <>
+      <div className="heading" id="heading">
+        <nav className="nav container">
+          <FontAwesomeIcon icon="fa-solid fa-cube" />
+          <div className="nav__menu" id="nav__menu">
+            <ul className="nav__list">
+              <li className="nav_item">
+                <a href="#home" className="nav__link active-link">
+                  Home
+                </a>
+              </li>
+              <li className="nav_item">
+                <a href="#home" className="nav__link">
+                  Featured
+                </a>
+              </li>
+              <li className="nav_item">
+                <a href="#home" className="nav__link">
+                  Products
+                </a>
+              </li>
+              <li className="nav_item">
+                <a href="#home" className="nav__link">
+                  New Collection
+                </a>
+              </li>
+            </ul>
 
-          <div className="nav__close" id="nav__close">
-            <i className="bx bx-x"></i>
+            <div className="nav__close" id="nav__close">
+              <i className="bx bx-x"></i>
+            </div>
           </div>
-        </div>
-        <div className="nav__btns">
-          <i className="bx bx-moon change-theme" id="theme-button"></i>
-          <div className="nav__shop" id="cart-shop">
-            <i className="bx bx-shopping-bag"></i>
+          <div className="nav__btns">
+            <FontAwesomeIcon icon="fa-sharp fa-solid fa-sun-bright" />
+            <div className="nav__shop" id="cart-shop">
+              <FontAwesomeIcon icon="fa-regular fa-basket-shopping-simple" />
+            </div>
+            <div className="nav__shop" id="nav-toggle">
+              <i className="bx bx-grid-alt"></i>
+            </div>
           </div>
-          <div className="nav__shop" id="nav-toggle">
-            <i className="bx bx-grid-alt"></i>
-          </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
+    </>
   );
 }
