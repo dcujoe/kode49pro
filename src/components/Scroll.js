026687036975c2f0f4c85d57694d653931d53871
Daniel Agbenyega <50689568+dcujoe@users.noreply.github.com>
2023-01-15 01:57:57 +0100

    updates

---
+++ src/components/Scroll.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./compocss/scroll.css";
+
+export default function Scroll() {
+  return (
+    <>
+      <a href="#" className="scrollup" id="scroll-up">
+        <i className="bx bx-up-arrow-alt scrollup__icon"></i>
+      </a>
+    </>
+  );
+}
