50979c5cd3635a1e6239df7f1c4542238ea178d9
Daniel Agbenyega <50689568+dcujoe@users.noreply.github.com>
2023-01-19 16:42:55 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -6,8 +6,22 @@ import Home from "./components/Home";
 import Special from "./components/Special";
 import Story from "./components/Story";
 import Variety from './components/Variety';
+/*import useSWR from 'swr'*/
 
 function App() {
+
+/*const fetcher = (...args) => fetch(...args).then(res => res.json())
+const { data, error, isLoading } = useSWR('', fetcher)
+
+  if (error) {
+    return <div>failed to load</div>
+}
+   else if(isLoading){  
+    return <div>loading...</div>}
+  else {
+  <div>hello {data.name}!</div>} */
+
+
     return ( 
         <div className = "App" >
         <Home />
