836b0ffc60e7d98d7738ce81ed623093bed801f0
Daniel Agbenyega <50689568+dcujoe@users.noreply.github.com>
2023-01-23 15:02:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import Head from "./components/Head";
 import Home from "./components/Home";
-import Featured from './components/Featured';
+import Featured from "./components/Featured";
 import Story from "./components/Story";
-import Variety from './components/Variety';
+import Variety from "./components/Variety";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 /*import useSWR from 'swr'*/
 
 function App() {
-
-
-    return ( 
-        <div className = "App" >
-        <Home />
-        <Head />
-        <Featured />
-        <Story />
-        <Variety />
-        <Footer />
-        </div>
-       
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Head />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Featured />} />
+          <Route path="/" element={<Story />} />
+          <Route path="/" element={<Variety />} />
+          <Route path="/" element={<Footer />} />
+          <Route path="/checkout" element={<checkout />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
