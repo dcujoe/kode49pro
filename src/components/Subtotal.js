3c49a26f6e6b9672d0090a9dc2bee6875c9d8fda
Daniel Agbenyega <50689568+dcujoe@users.noreply.github.com>
2023-01-23 11:26:51 +0100

    updates

---
+++ src/components/Subtotal.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./compocss/subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "reducer";
+import { useStateValue } from "./StateProvider";
+
+const Subtotal = () => {
+  {
+    /*gets the basket into the data layer*/
+  }
+  const [{ basket }, dispatch] = useStateValue();
+
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal({basket.length} items): <strong>€{value}</strong>
+            </p>
+            <small className="subtotal-gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={getbasketTotal(basket)}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"€"}
+      />
+      <button>Proceed to Checkout</button>
+    </div>
+  );
+};
+
+export default Subtotal;
---
+++ src/components/reducer.js
@@ -0,0 +1,48 @@
+// Here we define all the application level states and define actions to make state changes
+// A stateprovider is created as a component and data is retrieved from statelevel to component and application level
+
+// selector function. stipulates the total basket and what the basket should contain
+
+export const initialState = {
+  basket: [],
+};
+
+export const getBasketTotal = (basket) => {
+  // the .reduce function to return item and amount
+  return basket.reduce((amount, item) => item.price + amount);
+};
+
+//the reduce is immutable thus using const
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_BASKET":
+      return {
+        //returns the initial state indicated by ...state
+        ...state,
+        // adds to the initial state, the basket and action.item
+        basket: [...state.basket, action.item],
+      };
+    case "REMOVE_FROM_BASKET":
+      // identifies the state of the basket
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+
+      let newBasket = [...state.basket];
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't remove the product(id: ${action.id}) as its not in the basket!`
+        );
+      }
+      return {
+        ...state,
+        basket: newBasket,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
