d1029ff8507bcf5a694eb45da47c596c21e96cdd
Daniel Agbenyega <50689568+dcujoe@users.noreply.github.com>
2023-01-25 03:30:58 +0100

    Update Subtotal.js

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -5,31 +5,26 @@ import { getBasketTotal } from "./reducer";
 import { useStateValue } from "./StateProvider";
 
 const Subtotal = () => {
-  {
-    /*gets the basket into the data layer*/
-  }
   const [{ basket }, dispatch] = useStateValue();
-
-  const priceSplitter = (number) =>
-    number && number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
   return (
     <div className="subtotal">
-      {priceSplitter(
+      <CurrencyFormat
         renderText={(value) => (
-          
+          <>
             <p>
               Subtotal({basket.length} items): <strong>€{value}</strong>
             </p>
             <small className="subtotal-gift">
               <input type="checkbox" /> This order contains a gift
             </small>
+          </>
         )}
         decimalScale={2}
         value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
-      )}
+      />
       <button className="sub_button">Proceed to Checkout</button>
     </div>
   );
